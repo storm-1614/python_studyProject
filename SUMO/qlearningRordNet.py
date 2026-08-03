965db323a604b51f98abcd86b5908bc06174a1a3
storm-1614 <storm1614@outlook.com>
2026-08-03 10:49:48 +0800

    qlearning model note

--- SUMO/qlearningRordNet.py
+++ SUMO/qlearningRordNet.py
@@ -40,13 +40,19 @@ class TwoPhaseIntersection(gym.Env):
 
         # 奖励 + 终止状态
         reward = sum(self.queue) * -1
-        terminated = False
-        truncated = False
-        info = {}
+        terminated = False # 终止
+        truncated = False  # 截断
+        info = {} # 附加调试信息字典
 
         return self._obs(), reward, terminated, truncated, info
 
     def _obs(self):
+        """
+        return: 
+        queue[0] 方向 A 排队数
+        queue[1] 方向 B 排队数
+        phase    绿灯相位
+        """
         return np.array([self.queue[0], self.queue[1], self.phase], dtype=int)
 
 
@@ -54,6 +60,12 @@ class QLearning:
     def __init__(
         self, n_states=(5, 5, 2), n_actions=2, alpha=0.1, gamma=0.9, espilon=0.3
     ):
+        """
+        Q 表：
+        把两个元组拼起来，然后生成一张 4 维全零表
+        (5,5,2) + (2,) -> (5, 5, 2, 2)
+        变成：[queue0, queue1, phase, action]
+        """
         self.qTable = np.zeros(n_states + (n_actions,))
         self.alpha = alpha
         self.gamma = gamma
